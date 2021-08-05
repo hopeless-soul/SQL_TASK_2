8d756c52450a12560ad593a92eeb6903fe46dfff
hopeless-soul <r3al.hum4n@gmail.com>
2021-08-05 15:20:22 +0300

    Add KNEX to a database folder

--- db/index.js
+++ db/index.js
@@ -1,5 +1,16 @@
 const {Pool, Client} = require('pg');
 
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        database : 'tasks',
+        host : '127.0.0.1',
+        port: 5432,
+        user : 'user1',
+        password : '1'
+    }
+});
+
 const pool_birthdays = new Pool({
     user: 'user1',
     host: 'localhost',
@@ -19,3 +30,4 @@ const pool_tasks = new Pool({
 
 module.exports.poolBirthdays = pool_birthdays;
 module.exports.poolTasks = pool_tasks;
+module.exports.knex = knex;
